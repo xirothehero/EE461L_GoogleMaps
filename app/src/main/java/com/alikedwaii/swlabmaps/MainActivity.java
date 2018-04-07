b867d2f2b2978e17a0b9f0ee2584b4789d2d47f5
Ali Kedwaii <ali.kedwaii@utexas.edu>
2018-04-06 23:56:27 -0500

    v2 - can search two locations

--- app/src/main/java/com/alikedwaii/swlabmaps/MainActivity.java
+++ app/src/main/java/com/alikedwaii/swlabmaps/MainActivity.java
@@ -20,10 +20,13 @@ public class MainActivity extends AppCompatActivity {
     String query;
     String key;
     EditText searchbar;
+    EditText secondsearch;
     Geocoder geocoder;
     List<Address> addressList;
     Address addr;
-    double latitude, longitude;
+    double latitude1, longitude1;
+    double latitude2, longitude2;
+    String feature1, feature2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +34,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         searchbar = findViewById(R.id.search_bar);
+        secondsearch = findViewById(R.id.second_search);
     }
 
     public void locationSearch(View view){
         location = searchbar.getText().toString();
-        //location = location.replace(' ', '+');
-        //API query
-        key = getString(R.string.google_maps_key);
-        query = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=" + key;
         //get coords
         geocoder = new Geocoder(this);
         try {
@@ -47,14 +47,56 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
 
         }
-        latitude = addr.getLatitude();
-        longitude = addr.getLongitude();
+        latitude1 = addr.getLatitude();
+        longitude1 = addr.getLongitude();
+        feature1 = addr.getPhone();
         //send Intent to map
         Intent intent;
         intent = new Intent(MainActivity.this, MapsActivity.class);
         //add coords to bundle
-        intent.putExtra("latitude", latitude);
-        intent.putExtra("longitude", longitude);
+        intent.putExtra("latitude1", latitude1);
+        intent.putExtra("longitude1", longitude1);
+        intent.putExtra("feature1", feature1);
+        startActivity(intent);
+    }
+
+    public void doubleSearch(View view){
+        location = searchbar.getText().toString();
+        //get coords
+        geocoder = new Geocoder(this);
+        try {
+            addressList = geocoder.getFromLocationName(location, 1);
+            addr = addressList.get(0);
+        } catch (IOException e) {
+
+        }
+        latitude1 = addr.getLatitude();
+        longitude1 = addr.getLongitude();
+        feature1 = addr.getLocality();
+
+        location = secondsearch.getText().toString();
+        //get coords
+        geocoder = new Geocoder(this);
+        try {
+            addressList = geocoder.getFromLocationName(location, 1);
+            addr = addressList.get(0);
+        } catch (IOException e) {
+
+        }
+        latitude2 = addr.getLatitude();
+        longitude2 = addr.getLongitude();
+        feature2 = addr.getLocality();
+
+        //send Intent to map
+        Intent intent;
+        intent = new Intent(MainActivity.this, MapsActivity.class);
+        //add coords to bundle
+        intent.putExtra("latitude1", latitude1);
+        intent.putExtra("longitude1", longitude1);
+        intent.putExtra("feature1", feature1);
+        intent.putExtra("latitude2", latitude2);
+        intent.putExtra("longitude2", longitude2);
+        intent.putExtra("feature2", feature2);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/alikedwaii/swlabmaps/MapsActivity.java
+++ app/src/main/java/com/alikedwaii/swlabmaps/MapsActivity.java
@@ -1,8 +1,10 @@
 package com.alikedwaii.swlabmaps;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,12 +13,25 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.w3c.dom.Text;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
-    private double latitude;
-    private double longitude;
+    private TextView feature1;
+    private TextView feature2;
+    private TextView distance;
+
+    private boolean has2;
+
+    private String f1, f2;
+
+    private double latitude1;
+    private double longitude1;
+
+    private double latitude2;
+    private double longitude2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +42,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        feature1 = findViewById(R.id.feature1);
+        feature2 = findViewById(R.id.feature2);
+        distance = findViewById(R.id.distance);
+
+        has2 = false;
         //set lat and long
         Intent intent = getIntent();
-        latitude = intent.getDoubleExtra("latitude", 0.00);
-        longitude = intent.getDoubleExtra("longitude", 0.00);
+        latitude1 = intent.getDoubleExtra("latitude1", 0.00);
+        longitude1 = intent.getDoubleExtra("longitude1", 0.00);
+        f1 = intent.getStringExtra("feature1");
+        if (intent.hasExtra("latitude2")) {
+            latitude2 = intent.getDoubleExtra("latitude2", 0.00);
+            has2 = true;
+        }
+        if (intent.hasExtra("longitude2")) longitude2 = intent.getDoubleExtra("longitude2", 0.00);
+        if (intent.hasExtra("feature2")) f2 = intent.getStringExtra("feature2");
     }
 
 
@@ -47,10 +74,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng newLocation = new LatLng(latitude, longitude);
-        mMap.addMarker(new MarkerOptions().position(newLocation).title("Marker"));
+        // Add a marker in location and move the camera
+        LatLng newLocation = new LatLng(latitude1, longitude1);
+        mMap.addMarker(new MarkerOptions().position(newLocation).title("Marker 1"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(newLocation));
+        mMap.setMinZoomPreference(3);
+        if(f1 != null) {
+            feature1.setText(f1);
+        }
+
+        if (has2){
+            LatLng newLocation2 = new LatLng(latitude2, longitude2);
+            mMap.addMarker(new MarkerOptions().position(newLocation2).title("Marker 2"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(newLocation2));
+            if(f2 != null) {
+                feature2.setText(f2);
+            }
+
+            float[] result = new float[1];
+            Location.distanceBetween(latitude1, longitude1,
+                    longitude2, longitude2,
+                    result);
+            String distText = "Distance between locations: " + String.valueOf(result[0]) + " m";
+            distance.setText(distText);
+        }
+
         
     }
 }
